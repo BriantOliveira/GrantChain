0c476ec71d810502f40f8b59e7505cfdd781d330
jaebooker <jaesonbooker@gmail.com>
2020-02-15 02:59:33 -0700

    trying ost

--- services/ost.js
+++ services/ost.js
@@ -1,9 +1,10 @@
 const OSTSDK = require('@ostdotcom/ost-sdk-js');
-
+const baseUrl = "https://api.ost.com/testnet/v2/";
+const tokenUrl = "tokens";
 const ostObj = new OSTSDK({
                             apiKey: api_key,
                             apiSecret: api_secret,
-                            apiEndpoint: localhost:3000/,
+                            apiEndpoint: baseUrl + tokenUrl,
                             config: {timeout: 10000}
                         });
 const tokensService = ostObj.services.tokens;
